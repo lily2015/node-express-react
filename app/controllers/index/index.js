201ea599307bac896c2bb17ee157856ce10b15d4
lily2015 <lixue2015@hotmail.com>
2016-01-19 13:28:47 +0800

    头部变量传参

--- app/controllers/index/index.js
+++ app/controllers/index/index.js
@@ -1,11 +1,14 @@
 var React = require('react')
   , ReactDOMServer = require('react-dom/server')
+  , config = require('../../config/config')
   , Loader = React.createFactory(require("../../react/index/demo"));
 
 // index page
 exports.index = function(req, res, next) {
-
+  var docInfo = {
+    "config": config,
+    "title": "商城首页"
+  };
   var reactHtml = ReactDOMServer.renderToString(React.createElement('h1', null, 'Hello, world!!'));
-  
-  res.render('index', {reactOutput: reactHtml});
+  res.render('index', {reactOutput: reactHtml, docInfo: docInfo});
 }
