1ae71e1c8308ae4114d105117f048e2a74b99088
lily2015 <lixue2015@hotmail.com>
2016-02-02 14:27:14 +0800

    socket.io 引用；but WebSocket connection to""failed: Connection closed before receiving a handshake response

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
 var express = require('express')
   , app = express()
   , config = require('./app/config/config')
-  , http = require('http').Server(app)
-  , io = require('socket.io')(http);
+  , server = require('http').createServer(app)
+  , io = require('socket.io')(server);
 
 // express settings
 require('./app/config/express')(app, config, io);
@@ -19,7 +19,7 @@ var env = process.env.NODE_ENV || 'dev';
 // Start the app by listening on <port>
 var port = process.env.PORT || 3050;
 
-http.listen(port);
+server.listen(port);
 console.log('Express app started on port ' + port + " || config environment is " + env); 
 // expose app
 exports = module.exports = app;
--- app/controllers/index/index.js
+++ app/controllers/index/index.js
@@ -33,13 +33,17 @@ exports.index = function(app, io){
     params_mall_list.apiName = 'Service/callback.mi/ECommerce/RecommendProducts.api';
     params_mall_list.apiParams = {};
     var res_mall_list = new rq(params_mall_list);
-    io.on('connection', function(socket){
-      socket.on('more list', function(params){
+    io.sockets.on('connection', function(socket){
+      /*socket.on('more list', function(params){
         params_mall_list.apiParams = params;
         res_mall_list._rq(function(d_mall_list){
           var reactMallList = ReactDOMServer.renderToString(React.createElement(FilmTicketBox, {data: d_mall_list}));
         });
         io.emit('more list', 'reactMallList');
+      });*/
+      socket.emit('news', { hello: 'world' });
+      socket.on('my other event', function (data) {
+        console.log(data);
       });
     });
   });
