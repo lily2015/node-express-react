e647ed4c76d115f36f0ef87bbda8bfe42b78e473
lily2015 <lixue2015@hotmail.com>
2016-02-14 15:07:30 +0800

    日志调整

--- app.js
+++ app.js
@@ -4,13 +4,14 @@
 
 var express = require('express')
   , app = express()
-  , config = require('./app/config/config');
+  , config = require('./app/config/config')
+  , loggerApp = require('./app/config/logs').loggerApp;
 
 // express settings
 require('./app/config/express')(app, config);
 
 // logs
-require('./app/config/logs').logApp(app);
+// require('./app/config/logs').logApp(app);
 
 var env = process.env.NODE_ENV || 'dev';
 
@@ -18,7 +19,8 @@ var env = process.env.NODE_ENV || 'dev';
 var port = process.env.PORT || 3050;
 
 app.listen(port);
-console.log('Express app started on port ' + port + " || config environment is " + env); 
+console.log('Express app started on port ' + port + " || config environment is " + env);
+loggerApp.info("pid=" + process.pid + "|env=" + env + "|port=" + port);
 // expose app
 exports = module.exports = app;
 
--- app/config/logs.js
+++ app/config/logs.js
@@ -1,23 +1,15 @@
-var config = require('./config');
-var log4js = require('log4js');
-var log4js_config = require("./log4js.json");
+var config = require('./config')
+  , log4js = require('log4js')
+  , log4js_config = require("./log4js.json");
+
 log4js.configure(log4js_config, {
   cwd: config.logsPath
 });
 // getLogger
-var _logApp = log4js.getLogger('logApp');
-var _logDaily = log4js.getLogger('logDaily');
-
-function logApp() {
-  var _this = this;
-  var port = process.env.PORT || 3050;
-  var env = process.env.NODE_ENV || 'dev';
-  // logs start info output
-  _logApp.info("pid=" + process.pid + "|env=" + env + "|port=" + port);
-};
+var _logApp = log4js.getLogger('logApp')
+  , _logDaily = log4js.getLogger('logDaily');
 
 module.exports = {
-  logApp: logApp,
   loggerApp: _logApp,
   loggerDaily: _logDaily
 };
